43f10680c2dda8a6f6ba5946ec5eda740e4f26ee
Maurine Duet <maurineduet@gmail.com>
2023-01-22 18:47:33 +0100

    Modification du js

--- js/animation.js
+++ js/animation.js
@@ -1,17 +1,39 @@
 window.addEventListener("scroll", reveal)
 
 function reveal() {
-    let reveals = document.querySelectorAll("[reveal-fade]")
-
-    for(let i = 0; i < reveals.length; i++) {
+    const reveals = document.querySelectorAll("[reveal-fade]")
+    
+    reveals.forEach(reveal => {
         let windowheight = window.innerHeight
-        let revealTop = reveals[i].getBoundingClientRect().top
+        let revealTop = reveal.getBoundingClientRect().top
         let revealPoint = 150
-
+        
         if(revealTop < windowheight - revealPoint) {
-            reveals[i].classList.add("active")
+            reveal.classList.add("active")
         } else {
-            reveals[i].classList.remove("active")
+            reveal.classList.remove("active")
         }
-    }
+        
+    })
+
 }
+
+/*
+for(let i = 0; i < reveals.length; i++) {
+    let windowheight = window.innerHeight
+    let revealTop = reveals[i].getBoundingClientRect().top
+    let revealPoint = 150
+    
+    if(revealTop < windowheight - revealPoint) {
+        reveals[i].classList.add("active")
+    } else {
+        reveals[i].classList.remove("active")
+    }
+    
+} */
+
+
+
+
+
+
